95d76a5ce6ee2313e3ae600c92f9ebd5b6939035
vishnu yennam <vishnucharan.yennam@live.edu.au>
2022-03-03 20:59:00 +1100

    First Assignment

---
+++ class1.R
@@ -0,0 +1,13 @@
+153/3
+a <- 10
+b<-"cool"
+c<-c(2,3,4,5)
+summary(c)
+summary(c(3,4,5,6))
+install.packages("tidyverse")
+library(tidyverse)
+data(mtcars)
+summary(mtcars)
+hear(mtcars)
+glimpse(mtcars)
+
